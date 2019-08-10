3ecde61bb8d24c6fcb524416a9c3eccbdcbc91e1
delphinus <me@delphinus.dev>
2019-08-10 16:48:16 +0900

    Make user-defined dict be typed

--- iterm2_battery.py
+++ iterm2_battery.py
@@ -8,9 +8,27 @@ from iterm2.statusbar import Knob
 from math import floor
 from pathlib import Path
 from typing import Any, Callable, List, TypeVar, cast
+from typing_extensions import Protocol
+from mypy_extensions import TypedDict
 
 # ref https://github.com/python/mypy/issues/1551#issuecomment-253978622
 TFun = TypeVar("TFun", bound=Callable[..., Any])
+BatteryInfo = TypedDict(
+    "BatteryInfo",
+    {
+        "elapsed": int,
+        "error": str,
+        "is_calculating": bool,
+        "is_charging": bool,
+        "percent": int,
+        "status": str,
+    },
+)
+
+
+class battery(Protocol):
+    def result(self) -> BatteryInfo:
+        ...
 
 
 class battery_info(Structure):
@@ -21,6 +39,26 @@ class battery_info(Structure):
         ("status", c_char_p),
         ("error", c_char_p),
     ]
+
+
+class lib_caller:
+    path = Path(__file__).resolve().parent / "battery.so"
+
+    def __init__(self) -> None:
+        self.lib = cdll.LoadLibrary(str(lib_caller.path))
+        self.lib.battery.restype = battery_info
+        self.lib.battery.argtypes = ()
+
+    def result(self) -> BatteryInfo:
+        res = self.lib.battery()
+        return {
+            "elapsed": max(res.elapsed, res.charging),
+            "error": res.error,
+            "is_calculating": res.charging == -1,
+            "is_charging": res.charging == 1,
+            "percent": res.percent,
+            "status": res.status,
+        }
 
 
 class memoize:
@@ -45,7 +83,7 @@ class memoize:
         return cast(TFun, wrapper)
 
 
-class battery:
+class battery_component:
     chars = ["▏", "▎", "▍", "▌", "▋", "▊", "▉", "█"]
     thunder = "ϟ"
     width = 5
@@ -53,49 +91,49 @@ class battery:
     icon = "🔋"
     plugged = "🔌"
 
-    def __init__(self) -> None:
-        self.lib = cdll.LoadLibrary(str(battery.lib_path))
-        self.lib.battery.restype = battery_info
-        self.lib.battery.argtypes = ()
+    def __init__(self, bat: battery) -> None:
+        self.battery = bat
 
     def status(self) -> str:
-        result = self.lib.battery()
-        if result.error is not None:
-            return battery.plugged
+        result: BatteryInfo = self.battery.result()
+        if result["error"] is not None:
+            return battery_component.plugged
 
-        bat = self._battery(result.status, result.percent)
-        time = self._time(result.elapsed, result.charging)
-        return f"{battery.icon} |{bat}| {result.percent:d}%{time}"
+        bat = self._battery(result["status"], result["percent"])
+        time = self._time(
+            result["elapsed"], result["is_calculating"], result["is_charging"]
+        )
+        return f"{battery.icon} |{bat}| {result['percent']:d}%{time}"
 
     def _battery(self, status: str, percent: int) -> str:
         if status == b"AC Power" and percent == 100:
-            return battery.width * battery.chars[-1]
+            return battery_component.width * battery_component.chars[-1]
         elif status == b"AC Power":
-            mid: int = floor(battery.width / 2)
-            return mid * " " + battery.thunder + (battery.width - mid - 1) * " "
+            mid: int = floor(battery_component.width / 2)
+            return (
+                mid * " "
+                + battery_component.thunder
+                + (battery_component.width - mid - 1) * " "
+            )
         elif status == b"Battery Power":
-            unit: int = len(battery.chars)
-            total_char_len: int = len(battery.chars) * battery.width
+            unit: int = len(battery_component.chars)
+            total_char_len: int = len(battery_component.chars) * battery_component.width
             char_len: int = floor(total_char_len * percent / 100)
             full_len: int = floor(char_len / unit)
             remained: int = char_len % unit
-            space_len = battery.width - full_len - (0 if remained == 0 else 1)
-            bat = battery.chars[-1] * full_len
+            space_len = battery_component.width - full_len - (0 if remained == 0 else 1)
+            bat = battery_component.chars[-1] * full_len
             if remained != 0:
-                bat += battery.chars[remained - 1]
+                bat += battery_component.chars[remained - 1]
             bat += " " * space_len
             return bat
 
-        return " " * battery.width
+        return " " * battery_component.width
 
-    def _time(self, elapsed: int, charging: int) -> str:
-        if elapsed == 0 and charging == 0:
-            return ""
-        elif elapsed == -1 or charging == -1:
-            return " -:- "  # calculating
-
-        seconds = max(elapsed, charging)
-        return " {0:d}:{1:02d}".format(*divmod(seconds, 60))
+    def _time(self, elapsed: int, is_calculating: bool, is_charging: bool) -> str:
+        if is_calculating:
+            return " -:- "
+        return " {0:d}:{1:02d}".format(*divmod(elapsed, 60)) if elapsed > 0 else ""
 
 
 async def main(connection: Connection) -> None:
@@ -107,7 +145,7 @@ async def main(connection: Connection) -> None:
         30,
         "cx.remora.battery",
     )
-    bat = battery()
+    bat = battery_component(lib_caller())
 
     @StatusBarRPC
     async def battery_status(knobs: List[Knob]) -> str:
